39ca988df2184cb7086cefadc8a046ad887b5731
Shubham Sethi <shubhamsethi89@gmail.com>
2020-06-30 10:55:22 +0530

    Make After method uncommented

--- src/test/java/com/qa/testcases/ShoppingCartPageTest.java
+++ src/test/java/com/qa/testcases/ShoppingCartPageTest.java
@@ -1,6 +1,7 @@
 package com.qa.testcases;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -46,8 +47,8 @@ public class ShoppingCartPageTest extends TestBase{
 	}
 		
 	
-//	@AfterMethod
-//	public void tearDown(){
-//		driver.quit();
-//	}
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
 }
